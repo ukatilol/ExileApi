e55cfba2a36102e081216e5adaa813f0b1d77e79
TehCheat <TehCheat@users.noreply.github.com>
2020-07-09 18:54:52 -0400

    Fixed the rest of the IngameState offsets

--- GameOffsets/IngameStateOffsets.cs
+++ GameOffsets/IngameStateOffsets.cs
@@ -10,22 +10,22 @@ namespace GameOffsets
 		[FieldOffset(0x4F8)] public long Data;
         [FieldOffset(0x500)] public long ServerData;
         [FieldOffset(0x628)] public long UIRoot;
-		[FieldOffset(0x5E0)] public long UIHoverTooltip;
-		[FieldOffset(0x5E8)] public float CurentUElementPosX;
-		[FieldOffset(0x5EC)] public float CurentUElementPosY;
-		[FieldOffset(0x5F0)] public long UIHover;
-		[FieldOffset(0x618)] public int MouseXGlobal;
-		[FieldOffset(0x61C)] public int MouseYGlobal;
-		[FieldOffset(0x624)] public float UIHoverX;
-		[FieldOffset(0x628)] public float UIHoverY;
-		[FieldOffset(0x62C)] public float MouseXInGame;
-		[FieldOffset(0x630)] public float MouseYInGame;
-		[FieldOffset(0x654)] public float TimeInGame;
-		[FieldOffset(0x65C)] public float TimeInGameF;
-		[FieldOffset(0x670)] public int DiagnosticInfoType;
-        [FieldOffset(0x8A0)] public long LatencyRectangle;
-        [FieldOffset(0xD40)] public long FrameTimeRectangle;
-        [FieldOffset(0xF90)] public long FPSRectangle;
+		[FieldOffset(0x660)] public long UIHoverTooltip;
+		[FieldOffset(0x668)] public float CurentUElementPosX;
+		[FieldOffset(0x66C)] public float CurentUElementPosY;
+		[FieldOffset(0x670)] public long UIHover;
+		[FieldOffset(0x698)] public int MouseXGlobal;
+		[FieldOffset(0x69C)] public int MouseYGlobal;
+		[FieldOffset(0x6A4)] public float UIHoverX;
+		[FieldOffset(0x6A8)] public float UIHoverY;
+		[FieldOffset(0x6AC)] public float MouseXInGame;
+		[FieldOffset(0x6B0)] public float MouseYInGame;
+		[FieldOffset(0x6D4)] public float TimeInGame;
+		[FieldOffset(0x6DC)] public float TimeInGameF;
+		[FieldOffset(0x6F0)] public int DiagnosticInfoType;
+        [FieldOffset(0x920)] public long LatencyRectangle;
+        [FieldOffset(0xDC0)] public long FrameTimeRectangle;
+        [FieldOffset(0x1010)] public long FPSRectangle;
         [FieldOffset(0x1160)] public int Camera;
     }
 }
