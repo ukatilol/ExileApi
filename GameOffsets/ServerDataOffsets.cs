1c88731a9a78cc902d1ee37498d4ee36877e84b3
DetectiveSquirrel <ratman7539@windowslive.com>
2020-08-08 15:55:26 +1200

    Revert "Update ServerDataOffsets.cs"
    This reverts commit da4677afa8a30a7bc2293d99d8bfbf8d2697e941.

--- GameOffsets/ServerDataOffsets.cs
+++ GameOffsets/ServerDataOffsets.cs
@@ -57,7 +57,7 @@ namespace GameOffsets
         [FieldOffset(0x7B38 - Skip)] public long AwakenedAreas;
         [FieldOffset(0x85E4 - Skip)] public byte MonsterLevel;
         [FieldOffset(0x85e5 - Skip)] public byte MonstersRemaining;
-        [FieldOffset(0x86A0 - Skip)] public ushort CurrentSulphiteAmount;
-        [FieldOffset(0x8690 - Skip)] public int CurrentAzuriteAmount;
+        [FieldOffset(0x86A0 - Skip)] public ushort CurrentSulphiteAmount; //Maybe wrong not tested
+        [FieldOffset(0x86A4 - Skip)] public int CurrentAzuriteAmount;
     }
 }
