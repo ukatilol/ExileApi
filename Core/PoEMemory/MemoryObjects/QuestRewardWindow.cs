395026e5cf861590200a5287209fa2a7bf0f16c8
Cr33zz <pawel.creez.kubiak@gmail.com>
2020-08-16 13:43:43 -0400

    Added quest rewards details.

--- Core/PoEMemory/MemoryObjects/QuestRewardWindow.cs
+++ Core/PoEMemory/MemoryObjects/QuestRewardWindow.cs
@@ -9,8 +9,12 @@ namespace ExileCore.PoEMemory.MemoryObjects
 {
     public class QuestRewardWindow : Element
     {
-        private Element PossibleRewardsWrapper => GetChildAtIndex(5)?.GetChildAtIndex(0)?.GetChildAtIndex(0);
-        public IList<Element> PossibleRewards => PossibleRewardsWrapper?.Children;
+        private Element PossibleRewardsWrapper => GetChildAtIndex(5)?.GetChildAtIndex(0);
+        public IList<(Entity, Element)> GetPossibleRewards()
+        {
+            var rewardsElems = PossibleRewardsWrapper?.Children.SelectMany(x => x.Children).Select(x => x.GetChildFromIndices(0, 1)).ToList();
+            return rewardsElems.Select(x => (x?.ReadObject<Entity>(x.Address + 0x0388), x)).ToList();
+        }
         public Element CancelButton => GetChildAtIndex(3);
         public Element SelectOneRewardString => GetChildAtIndex(0);
     }
