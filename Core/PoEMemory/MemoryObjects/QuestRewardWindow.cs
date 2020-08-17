5a61cfa2ba638eb3fc4e12e4662349a82a07094a
Cr33zz <pawel.creez.kubiak@gmail.com>
2020-08-17 13:55:16 -0400

    Fixed quest reward elements.

--- Core/PoEMemory/MemoryObjects/QuestRewardWindow.cs
+++ Core/PoEMemory/MemoryObjects/QuestRewardWindow.cs
@@ -9,11 +9,17 @@ namespace ExileCore.PoEMemory.MemoryObjects
 {
     public class QuestRewardWindow : Element
     {
-        private Element PossibleRewardsWrapper => GetChildAtIndex(5)?.GetChildAtIndex(0);
         public IList<(Entity, Element)> GetPossibleRewards()
         {
-            var rewardsElems = PossibleRewardsWrapper?.Children.SelectMany(x => x.Children).Select(x => x.GetChildFromIndices(0, 1)).ToList();
-            return rewardsElems.Select(x => (x?.ReadObject<Entity>(x.Address + 0x0388), x)).ToList();
+            var rewardsElems = GetChildAtIndex(5)?.GetChildAtIndex(0)?.Children.SelectMany(x => x.Children).ToList();
+            var rewards = new List<(Entity, Element)>();
+            foreach (var rewardElem in rewardsElems)
+            {
+                var subRewardElem = rewardElem.GetChildFromIndices(0, 1);
+                var rewardEntity = subRewardElem?.ReadObject<Entity>(subRewardElem.Address + 0x0388);
+                rewards.Add((rewardEntity, rewardElem));
+            }
+            return rewards;
         }
         public Element CancelButton => GetChildAtIndex(3);
         public Element SelectOneRewardString => GetChildAtIndex(0);
